5df87fbb3188e3c21146da74deee9f979d648fca
UneAbeilleee <elies.b.pro@gmail.com>
2024-01-29 17:39:53 +0100

    Ajout du setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='{to_do_list}',
+    version='1.0',
+    author='{UneAbeilleee}',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
