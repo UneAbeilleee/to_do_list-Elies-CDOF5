dc9debf1bda84ff5d98800a0ed095ae5021be672
UneAbeilleee <elies.b.pro@gmail.com>
2024-01-29 18:05:52 +0100

    Ajout d'une database avec CSV

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
+import csv
+
 class ToDoList:
     def __init__(self):
         self.tasks = []
+        self.load_tasks()
+
+    def load_tasks(self):
+        try:
+            with open('tasks.csv', 'r', newline='') as file:
+                reader = csv.reader(file)
+                self.tasks = [{'task': row[0], 'completed': row[1] == 'True'} for row in reader]
+        except FileNotFoundError:
+            pass
+
+    def save_tasks(self):
+        with open('tasks.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            for task in self.tasks:
+                writer.writerow([task['task'], str(task['completed'])])
 
     def add_task(self, task):
-        self.tasks.append({"task": task, "completed": False})
+        self.tasks.append({'task': task, 'completed': False})
         print(f'Task "{task}" added.')
+        self.save_tasks()
 
     def delete_task(self, task_index):
         if 1 <= task_index <= len(self.tasks):
             deleted_task = self.tasks.pop(task_index - 1)
             print(f'Task "{deleted_task["task"]}" deleted.')
+            self.save_tasks()
         else:
             print("Invalid task index.")
 
     def complete_task(self, task_index):
         if 1 <= task_index <= len(self.tasks):
-            self.tasks[task_index - 1]["completed"] = True
+            self.tasks[task_index - 1]['completed'] = True
             print(f'Task "{self.tasks[task_index - 1]["task"]}" marked as completed.')
+            self.save_tasks()
         else:
             print("Invalid task index.")
 
@@ -26,7 +46,7 @@ class ToDoList:
         else:
             print('Tasks in the to-do list:')
             for idx, task in enumerate(self.tasks, start=1):
-                status = "Completed" if task["completed"] else "Not Completed"
+                status = "Completed" if task['completed'] else "Not Completed"
                 print(f'{idx}. {task["task"]} - {status}')
 
 
